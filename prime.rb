6f94db5c82b921d12be4d823fc7873070ba913d3
Trevor Russell <truss.42@gmail.com>
2018-06-13 20:51:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(num)
   elsif num == 0 or num % 2 == 0
     return false
   else
-    list = [2..num].collect{|number| number.even?}
+    list = [2..num].to_a.collect{|number| number.even?}
     i = 0
     while i < list.length - 1
       if num % list[i] == 0
